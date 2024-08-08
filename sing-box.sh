9510ededfb98240b8937929518ea886197a0d7bd
yyfalbl <walbl@2670819.xyz>
2024-08-09 01:26:05 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -1,25 +1,25 @@
-#！/bin/bash
+#!/bin/bash
 
 # 定义颜色
-re=“\033[0m”
-红色=“\033[1;91米”
-green=“\e[1;32米”
-黄色=“\e[1;33米”
-紫色=“\e[1;35米”
-red（） { echo -e “\e[1;91米$1\033[0米”; }
-green（） { echo -e “\e[1;32米$1\033[0米”; }
-yellow（） { echo -e “\e[1;33m$1\033[0m”; }
-purple（） { echo -e “\e[1;35米$1\033[0米”; }
-reading（） { read -p “$（红色 ”$1“）”： “$2”; }
+re="\033[0m"
+red="\033[1;91m"
+green="\e[1;32m"
+yellow="\e[1;33m"
+purple="\e[1;35m"
+red() { echo -e "\e[1;91m$1\033[0m"; }
+green() { echo -e "\e[1;32m$1\033[0m"; }
+yellow() { echo -e "\e[1;33m$1\033[0m"; }
+purple() { echo -e "\e[1;35m$1\033[0m"; }
+reading() { read -p "$(red "$1")" "$2"; }
 
-用户名=$（whoami）
-HOSTNAME=$（主机名）
-UUID_FILE=“$HOME/.singbox_uuid” # 定义 UUID 的存储位置
+USERNAME=$(whoami)
+HOSTNAME=$(hostname)
+UUID_FILE="$HOME/.singbox_uuid"  # Define a location to store the UUID
 
 # Check if UUID file exists
 if [ -f "$UUID_FILE" ]; then
     export UUID=$(cat "$UUID_FILE")  # Read the existing UUID
-还
+else
     export UUID=$(uuidgen)  # Generate a new UUID
     echo "$UUID" > "$UUID_FILE"  # Save the UUID to the file
 fi
@@ -28,64 +28,307 @@ export NEZHA_SERVER=${NEZHA_SERVER:-''}
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''}
 
-[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
-[ -d “$WORKDIR” ] ||（MKDIR -P “$WORKDIR” & CHMOD 777 “$WORKDIR”)
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="${HOME}/${USERNAME}"
+[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
 
 read_vless_port() {
-    虽然是真的; 做
-Reading “请输入vless-reality端口 （面板开放的tcp端口）： ” vless_port
-        if [[ “$vless_port” =~ ^[0-9]+$ ]] && [ “$vless_port” -ge 1 ] && [ “$vless_port” -le 65535 ]; 然后
-green “你的vless-reality端口为： $vless_port”
-            破
-        还
-yellow “输入错误，请重新输入面板开放的TCP端口”
+    while true; do
+        reading "请输入vless-reality端口 (面板开放的tcp端口): " vless_port
+        if [[ "$vless_port" =~ ^[0-9]+$ ]] && [ "$vless_port" -ge 1 ] && [ "$vless_port" -le 65535 ]; then
+            green "你的vless-reality端口为: $vless_port"
+            break
+        else
+            yellow "输入错误，请重新输入面板开放的TCP端口"
         fi
-    做
+    done
 }
 
 read_hy2_port() {
-    虽然是真的; 做
-阅读 “请输入hysteria2端口 （面板开放的UDP端口）： ” hy2_port
-        if [[ “$hy 2_port” =~ ^[0-9]+$ ]] && [ “$hy 2_port” -ge 1 ] && [ “$hy 2_port” -le 65535 ]; 然后
-绿色 “你的hysteria2端口为： $hy 2_port”
-            破
-        还
-yellow “输入错误，请重新输入面板开放的UDP端口”
+    while true; do
+        reading "请输入hysteria2端口 (面板开放的UDP端口): " hy2_port
+        if [[ "$hy2_port" =~ ^[0-9]+$ ]] && [ "$hy2_port" -ge 1 ] && [ "$hy2_port" -le 65535 ]; then
+            green "你的hysteria2端口为: $hy2_port"
+            break
+        else
+            yellow "输入错误，请重新输入面板开放的UDP端口"
         fi
-    做
+    done
 }
 
+# read_tuic_port() {
+#     while true; do
+#         reading "请输入Tuic端口 (面板开放的UDP端口): " tuic_port
+#         if [[ "$tuic_port" =~ ^[0-9]+$ ]] && [ "$tuic_port" -ge 1 ] && [ "$tuic_port" -le 65535 ]; then
+#             green "你的tuic端口为: $tuic_port"
+#             break
+#         else
+#             yellow "输入错误，请重新输入面板开放的UDP端口"
+#         fi
+#     done
+# }
 
 read_nz_variables() {
-  if [ -n “$NEZHA_SERVER” ] && [ -n “$NEZHA_PORT” ] && [ -n “$NEZHA_KEY”] ]; 然后
-绿色 “使用自定义变量哪吒运行哪吒探针”
-      返回
-  还
-阅读 “是否需要安装哪吒探针？【y/n】： ” nz_choice
-      [[ -z $nz_choice ]] && 返回
-      [[ “$nz_choice” ！= “y” & “$nz_choice” ！= “Y” ]] && return
-阅读“请输入哪吒探针域名或IP：”NEZHA_SERVER
-green “你的哪吒域名为： $NEZHA_SERVER”
-阅读 “请输入哪吒探针端口（回车跳过默认使用5555）：”NEZHA_PORT
-      [[ -z $NEZHA_PORT ]] && NEZHA_PORT=“5555”
-绿色 “你的哪吒端口为： $NEZHA_PORT”
-阅读 “请输入哪吒探针密钥：” NEZHA_KEY
-绿色 “你的哪吒密钥为： $NEZHA_KEY”
+  if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+      green "使用自定义变量哪吒运行哪吒探针"
+      return
+  else
+      reading "是否需要安装哪吒探针？【y/n】: " nz_choice
+      [[ -z $nz_choice ]] && return
+      [[ "$nz_choice" != "y" && "$nz_choice" != "Y" ]] && return
+      reading "请输入哪吒探针域名或ip：" NEZHA_SERVER
+      green "你的哪吒域名为: $NEZHA_SERVER"
+      reading "请输入哪吒探针端口（回车跳过默认使用5555）：" NEZHA_PORT
+      [[ -z $NEZHA_PORT ]] && NEZHA_PORT="5555"
+      green "你的哪吒端口为: $NEZHA_PORT"
+      reading "请输入哪吒探针密钥：" NEZHA_KEY
+      green "你的哪吒密钥为: $NEZHA_KEY"
   fi
 }
-“rule_set”： [
-“Geosite-Netflix”
-],
-“outbound”： “wireguard-out”
-},
+
+install_singbox() {
+echo -e "${yellow}本脚本同时二协议共存${purple}(vless-reality|hysteria2)${re}"
+echo -e "${yellow}开始运行前，请确保在面板${purple}已开放2个端口，一个tcp端口和一个udp端口${re}"
+echo -e "${yellow}面板${purple}Additional services中的Run your own applications${yellow}已开启为${purplw}Enabled${yellow}状态${re}"
+reading "\n确定继续安装吗？【y/n】: " choice
+  case "$choice" in
+    [Yy])
+        cd $WORKDIR
+        read_nz_variables
+        read_vless_port
+        read_hy2_port
+        # read_tuic_port
+        download_singbox && wait
+        generate_config
+        run_sb && sleep 3
+        get_links
+      ;;
+    [Nn]) exit 0 ;;
+    *) red "无效的选择，请输入y或n" && menu ;;
+  esac
+}
+
+uninstall_singbox() {
+  reading "\n确定要卸载吗？【y/n】: " choice
+    case "$choice" in
+       [Yy])
+          kill -9 $(ps aux | grep '[w]eb' | awk '{print $2}')
+          kill -9 $(ps aux | grep '[b]ot' | awk '{print $2}')
+          kill -9 $(ps aux | grep '[n]pm' | awk '{print $2}')
+          rm -rf $WORKDIR
+          ;;
+        [Nn]) exit 0 ;;
+        *) red "无效的选择，请输入y或n" && menu ;;
+    esac
+}
+
+# Download Dependency Files
+download_singbox() {
+  ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+  if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+      FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/sb web""https://github.com/eooce/test/releases/download/ARM/swith npm")
+  elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+      FILE_INFO=("https://eooce.2go.us.kg/web web" "https://eooce.2go.us.kg/npm npm")
+  else
+      echo "Unsupported architecture: $ARCH"
+      exit 1
+  fi
+  for entry in "${FILE_INFO[@]}"; do
+      URL=$(echo "$entry" | cut -d ' ' -f 1)
+      NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+      FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+      if [ -e "$FILENAME" ]; then
+          green "$FILENAME already exists, Skipping download"
+      else
+          wget -q -O "$FILENAME" "$URL"
+          green "Downloading $FILENAME"
+      fi
+      chmod +x $FILENAME
+  done
+}
+
+# Generating Configuration Files
+generate_config() {
+
+    output=$(./web generate reality-keypair)
+    private_key=$(echo "${output}" | awk '/PrivateKey:/ {print $2}')
+    public_key=$(echo "${output}" | awk '/PublicKey:/ {print $2}')
+
+    openssl ecparam -genkey -name prime256v1 -out "private.key"
+    openssl req -new -x509 -days 3650 -key "private.key" -out "cert.pem" -subj "/CN=$USERNAME.serv00.net"
+
+  cat > config.json << EOF
+{
+  "log": {
+    "disabled": true,
+    "level": "info",
+    "timestamp": true
+  },
+  "dns": {
+    "servers": [
       {
-“rule_set”： [
-“geosite-category-ads-all”
-],
+        "tag": "google",
+        "address": "tls://8.8.8.8",
+        "strategy": "ipv4_only",
+        "detour": "direct"
+      }
+    ],
+    "rules": [
+      {
+        "rule_set": [
+          "geosite-openai"
+        ],
+        "server": "wireguard"
+      },
+      {
+        "rule_set": [
+          "geosite-netflix"
+        ],
+        "server": "wireguard"
+      },
+      {
+        "rule_set": [
+          "geosite-category-ads-all"
+        ],
+        "server": "block"
+      }
+    ],
+    "final": "google",
+    "strategy": "",
+    "disable_cache": false,
+    "disable_expire": false
+  },
+    "inbounds": [
+    {
+       "tag": "hysteria-in",
+       "type": "hysteria2",
+       "listen": "::",
+       "listen_port": $hy2_port,
+       "users": [
+         {
+             "password": "$UUID"
+         }
+     ],
+     "masquerade": "https://bing.com",
+     "tls": {
+         "enabled": true,
+         "alpn": [
+             "h3"
+         ],
+         "certificate_path": "cert.pem",
+         "key_path": "private.key"
+        }
+    },
+    {
+        "tag": "vless-reality-vesion",
+        "type": "vless",
+        "listen": "::",
+        "listen_port": $vless_port,
+        "users": [
+            {
+              "uuid": "$UUID",
+              "flow": "xtls-rprx-vision"
+            }
+        ],
+        "tls": {
+            "enabled": true,
+            "server_name": "www.ups.com",
+            "reality": {
+                "enabled": true,
+                "handshake": {
+                    "server": "www.ups.com",
+                    "server_port": 443
+                },
+                "private_key": "$private_key",
+                "short_id": [
+                  ""
+                ]
+            }
+        }
+    }
+    # {
+    #   "tag": "tuic-in",
+    #   "type": "tuic",
+    #   "listen": "::",
+    #   "listen_port": $tuic_port,
+    #   "users": [
+    #     {
+    #       "uuid": "$UUID",
+    #       "password": "admin123"
+    #     }
+    #   ],
+    #   "congestion_control": "bbr",
+    #   "tls": {
+    #     "enabled": true,
+    #     "alpn": [
+    #       "h3"
+    #     ],
+    #     "certificate_path": "cert.pem",
+    #     "key_path": "private.key"
+    #   }
+    # }
+
+ ],
+    "outbounds": [
+    {
+      "type": "direct",
+      "tag": "direct"
+    },
+    {
+      "type": "block",
+      "tag": "block"
+    },
+    {
+      "type": "dns",
+      "tag": "dns-out"
+    },
+    {
+      "type": "wireguard",
+      "tag": "wireguard-out",
+      "server": "162.159.195.100",
+      "server_port": 4500,
+      "local_address": [
+        "172.16.0.2/32",
+        "2606:4700:110:83c7:b31f:5858:b3a8:c6b1/128"
+      ],
+      "private_key": "mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=",
+      "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+      "reserved": [
+        26,
+        21,
+        228
+      ]
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "protocol": "dns",
+        "outbound": "dns-out"
+      },
+      {
+        "ip_is_private": true,
+        "outbound": "direct"
+      },
+      {
+        "rule_set": [
+          "geosite-openai"
+        ],
+        "outbound": "wireguard-out"
+      },
+      {
+        "rule_set": [
+          "geosite-netflix"
+        ],
+        "outbound": "wireguard-out"
+      },
+      {
+        "rule_set": [
+          "geosite-category-ads-all"
+        ],
         "outbound": "block"
       }
-],
-“rule_set”： [
+    ],
+    "rule_set": [
       {
         "tag": "geosite-netflix",
         "type": "remote",
@@ -118,88 +361,83 @@ green “你的哪吒域名为： $NEZHA_SERVER”
     }
   }
 }
-EOF（英语：EOF）
+EOF
 }
 
-# 运行文件
+# running files
 run_sb() {
-  如果 [ -e npm ]; 然后
-tlsPorts=（“443”， “8443”， “2096”， “2087”， “2083”， “2053”)
-    if [[ “${tlsPorts[*]}” =~ “${NEZHA_PORT}” ]]; 然后
-NEZHA_TLS=“--tls”
-    还
-NEZHA_TLS=""
+  if [ -e npm ]; then
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
     fi
-    if [ -n “$NEZHA_SERVER” ] && [ -n “$NEZHA_PORT” ] && [ -n “$NEZHA_KEY”] ]; 然后
-        出口TMPDIR=$（pwd）
-nohup ./npm -s ${NEZHA_SERVER}：${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-睡眠 2
-pgrep -x “npm” > /dev/null && green “npm 正在运行” ||{ 红色 “npm 未运行，正在重新启动...“;pkill -x “npm” && nohup ./npm -s “${NEZHA_SERVER}：${NEZHA_PORT}” -p “${NEZHA_KEY}” ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2;紫色“npm 已重启”; }
-    还
-紫色“哪吒变量为空，正在跳过运行”
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+        export TMPDIR=$(pwd)
+        nohup ./npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+        sleep 2
+        pgrep -x "npm" > /dev/null && green "npm is running" || { red "npm is not running, restarting..."; pkill -x "npm" && nohup ./npm -s "${NEZHA_SERVER}:${NEZHA_PORT}" -p "${NEZHA_KEY}" ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; purple "npm restarted"; }
+    else
+        purple "NEZHA variable is empty,skiping runing"
     fi
   fi
 
-  如果 [ -e web ]; 然后
-nohup ./web run -c config.json >/dev/null 2>&1 &
-睡眠 2
-pgrep -x “web” > /dev/null && green “web is running” ||{ 红色 “网络未运行，正在重启...“;pkill -x “web” & nohup ./web run -c config.json >/dev/null 2>&1 & sleep 2;紫色的“Web 已重新启动”; }
+  if [ -e web ]; then
+    nohup ./web run -c config.json >/dev/null 2>&1 &
+    sleep 2
+    pgrep -x "web" > /dev/null && green "web is running" || { red "web is not running, restarting..."; pkill -x "web" && nohup ./web run -c config.json >/dev/null 2>&1 & sleep 2; purple "web restarted"; }
   fi
 
 }
 
 get_links(){
-# 获取 IP
-IP=$（curl -s ipv4.ip.sb || { ipv6=$（curl -s --max-time 1 ipv6.ip.sb）; 回声 “[$ipv 6]”;）
-睡眠 1
-# 获取 ipinfo
-ISP=$（curl -s https://speed.cloudflare.com/meta | awk -F\“ '{打印 $26”-“$18}' | sed -e 's/ /_/g'） 
-睡眠 1
-黄色 “注意：v2ray或其他软件的跳过证书验证需设置为true，否则hy2或tuic节点可能不通\n”
-猫 > list.txt <<EOF
-vless://$UUID@$IP：$vless_port？encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.ups.com&fp=chrome&pbk=$public_key&type=tcp&headerType=none#$ISP
+# get ip
+IP=$(curl -s ipv4.ip.sb || { ipv6=$(curl -s --max-time 1 ipv6.ip.sb); echo "[$ipv6]"; })
+sleep 1
+# get ipinfo
+ISP=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g') 
+sleep 1
+yellow "注意：v2ray或其他软件的跳过证书验证需设置为true,否则hy2或tuic节点可能不通\n"
+cat > list.txt <<EOF
+vless://$UUID@$IP:$vless_port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.ups.com&fp=chrome&pbk=$public_key&type=tcp&headerType=none#$ISP
 
-歇斯底里2：//$UUID@$IP：$hy 2_port/？sni=www.bing.com&alpn=h3&insecure=1#$ISP
+hysteria2://$UUID@$IP:$hy2_port/?sni=www.bing.com&alpn=h3&insecure=1#$ISP
 
-tuic://$UUID:admin123@$IP:$tuic_port?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#$ISP
-EOF（英语：EOF）
-猫list.txt
-紫色“list.txt成功保存”
-紫色的“跑步完成！
-睡眠 3
-rm -rf npm boot.log sb.log 核心
+EOF
+cat list.txt
+purple "list.txt saved successfully"
+purple "Running done!"
+sleep 3 
+rm -rf npm boot.log sb.log core
 
 }
 
 #主菜单
-菜单() {
-清楚
-   回波 ""
-紫色 “=== Serv00|sing-box一键安装脚本 ===\n”
-purple “=== 转载老王脚本，去除tuic协议，增加UUID自动生成 ===\n”
-   echo -e “${green}脚本地址：${re}${yellow}https://github.com/yyfalbl/singbox-2${re}\n”   
-   purple   "***转载请著名出处，请勿滥用***\n"
-   回波  "==============="
-绿色“1.安装sing-box”
-   回波  "==============="
-红色“2.卸载sing-box”
-   回波  "==============="
-绿色“3.查看节点信息”
-   回波  "==============="
-   yellow "4. 清理所有进程"
-   回波  "==============="
-红色“0。退出脚本”
-   回波 "==========="
-阅读 “请输入选择（0-3）： ” choice
-   回波 ""
-    大小写 “${choice}” 
-1） install_singbox ;;
-2） uninstall_singbox ;;
-3） 猫 $WORKDIR/list.txt ;;
-        4) kill_all_tasks ;;
-0） 退出 0 ;;
-        *) red "无效的选项，请输入 0 到 4" ;;
-    ESAC公司
+menu() {
+   clear
+   echo ""
+   purple "=== Serv00|sing-box一键安装脚本 ===\n"
+   purple "=== 转载老王脚本，去除tuic协议，增加UUID自动生成 ===\n"
+   echo -e "${green}脚本地址：${re}${yellow}https://github.com/eooce/Sing-box${re}\n"
+   purple "转载请著名出处，请勿滥用\n"
+   green "1. 安装sing-box"
+   echo  "==============="
+   red "2. 卸载sing-box"
+   echo  "==============="
+   green "3. 查看节点信息"
+   echo  "==============="
+   red "0. 退出脚本"
+   echo "==========="
+   reading "请输入选择(0-3): " choice
+   echo ""
+    case "${choice}" in
+        1) install_singbox ;;
+        2) uninstall_singbox ;; 
+        3) cat $WORKDIR/list.txt ;; 
+        0) exit 0 ;;
+        *) red "无效的选项，请输入 0 到 3" ;;
+    esac
 }
-菜单
+menu
 
