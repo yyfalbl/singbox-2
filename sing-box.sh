b8632e64f620e7dcf02515a0b83c46205c2a1419
yyfalbl <walbl@2670819.xyz>
2024-09-10 07:25:22 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -148,8 +148,8 @@ EOF
     echo -e "\033[1;3;33msocks://${SOCKS5_USER}:${SOCKS5_PASS}@${SERV_DOMAIN}:${SOCKS5_PORT}\033[0m"
     # 将 socks5 代理信息添加到 list.txt 文件中
     cat <<EOF >> "$WORKDIR/list.txt"
-Socks5 代理地址： $IP:$SOCKS5_PORT 用户名：$SOCKS5_USER 密码：$SOCKS5_PASS
-socks://${SOCKS5_USER}:${SOCKS5_PASS}@${SERV_DOMAIN}:${SOCKS5_PORT}
+\033[1;3;33mSocks5 代理地址： $IP:$SOCKS5_PORT 用户名：$SOCKS5_USER 密码：$SOCKS5_PASS\033[0m
+\033[1;3;33msocks://${SOCKS5_USER}:${SOCKS5_PASS}@${SERV_DOMAIN}:${SOCKS5_PORT}\033[0m
 EOF
   else
     echo -e "\033[1;3;31mSocks5 代理程序启动失败\033[0m"
