f59b2d1ba72d8f193cee512de18240329c5e9318
yyfalbl <fyun9177@gmail.com>
2024-09-11 18:09:19 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -27,14 +27,14 @@ cleanup_and_delete() {
 
     # 检查目录是否存在
     if [ -d "$target_dir" ]; then
-        echo "准备删除目录 $target_dir 中的内容，排除 $exclude_dir..."
+        echo "准备删除所有文件 $exclude_dir..."
 
         # 删除除 $exclude_dir 以外的所有内容
         find "$target_dir" -mindepth 1 -maxdepth 1 ! -name "$exclude_dir" -exec rm -rf {} + 2>/dev/null
 
         # 检查删除是否成功
         if [ -d "$target_dir/$exclude_dir" ] && [ ! "$(ls -A "$target_dir" | grep -v "$exclude_dir")" ]; then
-            echo "目录 $target_dir 中的内容已成功删除，但保留了 $exclude_dir。"
+            echo " $target_dir 所有文件已成功删除。"
         else
             echo "目录 $target_dir 删除时出现问题，请检查是否有权限问题或其他错误。"
         fi
