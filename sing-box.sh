34811d9e27e74c7bafa07766c4e32308f66186de
yyfalbl <fyun9177@gmail.com>
2024-09-15 05:33:54 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -56,7 +56,7 @@ get_login_url() {
 # 定义主函数
 process_ip() {
  # 确保用户已经输入了用户名和面板编号
-    if [[ -z "$username" || -z "$panel_number" ]]; then
+    if [[ -z "$password_file" || -z "$panel_number_file" ]]; then
         echo "用户名或面板编号未输入，无法获取备用 IP!"
         return
     fi
