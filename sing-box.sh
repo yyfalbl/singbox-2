3b7aa70e060c81e6551f642bea55c6265d3847e4
yyfalbl <fyun9177@gmail.com>
2024-09-16 18:28:31 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -124,7 +124,7 @@ process_ip() {
             
             # 提示用户是否重新尝试登录
          echo -n -e "\e[1;3;33m是否重新登录？（y/n）:\e[0m" 
-         read  choice
+         read -r choice
              if [[ "$choice" =~ ^[Nn]$ ]]; then
            echo -e "\e[1;3;31m退出登录流程。\e[0m"
                 return  # 用户选择不再登录时退出
