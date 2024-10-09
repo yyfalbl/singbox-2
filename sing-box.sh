14410c9aeb43cc25126b78c9f1c00324cb8d6dd3
yyfalbl <fyun9177@gmail.com>
2024-10-09 14:07:56 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -1682,6 +1682,7 @@ $(if [ "$INSTALL_VMESS" = "true" ]; then
    echo ""
     # 如果 ARGO_CONFIGURED 为 true，生成带 argodomain 的链接
     if [ "$ARGO_CONFIGURED" = true ]; then
+      echo "以下为vmess链接，替换speed.cloudflare.com为自己的优选ip可获得极致体验"
         printf "${YELLOW}\033[1mvmess://$(echo "{ \"v\": \"2\", \"ps\": \"${USERNAME}-${subdomain}\", \"add\": \"www.visa.com.tw\", \"port\": \"443\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"$argodomain\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)${RESET}\n"
     fi
 fi)
