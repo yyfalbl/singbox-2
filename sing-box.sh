0123420db6bd76abf9108151cacd86dab8047700
yyfalbl <fyun9177@gmail.com>
2024-11-11 22:48:53 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -1987,31 +1987,22 @@ WORKDIR="$HOME/sbox"
 
   # 启动 bot 进程
 if [ -e "$WORKDIR/bot" ]; then
-  # 设置 args 参数
-  if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-    args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
-  elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
-    args="tunnel --edge-ip-version auto --config $WORKDIR/tunnel.yml run"
-  elif [[ $ARGO_AUTH =~ ^\{.*\}$ ]]; then
-     args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --json ${ARGO_AUTH}"
-  else
-    # 默认使用本地转发配置，判断是否设置了 vmess_port
-if [[ -f "$WORKDIR/boot.log" ]]; then
-    # 从 boot.log 中提取域名
-    argodomain=$(grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' $WORKDIR/boot.log | sed 's@https://@@') 
-    # 从 boot.log 提取端口号
-    vmess_port=$(grep -oE 'localhost:([0-9]+)' "$WORKDIR/boot.log" | sed 's/localhost://')
-    # 如果同时提取到域名和端口号
-    if [[ -n "$argodomain" && -n "$vmess_port" ]]; then
-        # 使用提取的域名和端口
-        args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile $WORKDIR/boot.log --loglevel info --url http://localhost:$vmess_port --hostname $argodomain"
+    # 如果 ARGO_AUTH 是一个有效的 Token 格式
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+      args="${args:-tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}}"
+    # 如果 ARGO_AUTH 包含 TunnelSecret 字符串，表示可能是一个 JSON 格式的配置
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+      args="${args:-tunnel --edge-ip-version auto --config $WORKDIR/tunnel.yml run}"
+    # 如果 ARGO_AUTH 是有效的 JSON 格式（检查是否以 '{' 开头并包含 '}' 结尾）
+    elif [[ $ARGO_AUTH =~ ^\{.*\}$ ]]; then
+      args="${args:-tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --json ${ARGO_AUTH}}"
     else
-        args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile $WORKDIR/boot.log --loglevel info --url http://localhost:8080 "
+      # 默认配置，使用 http2 协议和本地转发
+      if [[ -n "$vmess_port" ]]; then
+      args="${args:-tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile $WORKDIR/boot.log --loglevel info --url http://localhost:$vmess_port}"
+    else
+      args="${args:-tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile $WORKDIR/boot.log --loglevel info --url http://localhost:8080}"
     fi
-
-fi
-
-
  fi
 
   # 启动 bot 进程
