1dc99c45294fca2147aad06cb5b68db0efc5cada
yyfalbl <fyun9177@gmail.com>
2024-11-29 00:27:21 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -1390,40 +1390,69 @@ generate_config() {
         echo "Error: No port number provided. Configuration file will not be generated."
         return 1
     fi
+    
+# 测试每个 DNS 的延迟
+for i in "${!dns_servers[@]}"; do
+    # 使用 ping 测试延迟
+    latency=$(ping -c 1 -W 1 "${dns_servers[i]}" 2>/dev/null | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1}')
+    
+    # 检查是否成功获取延迟值
+    if [[ $latency =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        latencies[i]=$latency
+      echo -e "\033[1;3;35m${dns_names[i]} DNS 延迟: ${latency}ms\033[0m"
+    else
+        latencies[i]=9999
+        echo "${dns_names[i]} DNS latency: Unreachable"
+    fi
+done
 
+# 找出最快的 DNS
+for i in "${!latencies[@]}"; do
+    if (( $(echo "${latencies[i]} < $min_latency" | bc -l) )); then
+        min_latency=${latencies[i]}
+        fastest_dns=${dns_names[i]}
+    fi
+done
+
+# 输出结果
+if [[ -n $fastest_dns ]]; then
+   echo -e "\033[1;3;33m最快的 DNS 是 ${fastest_dns}，延迟为 ${min_latency} 毫秒。\033[0m"
+else
+    echo -e "\033[1;3;31m找不到可访问的DNS。\033[0m"
+
+fi
     # 基于所选服务创建配置文件
     cat > "$WORKDIR/config.json" <<EOF
 {
   "log": {
-    "disabled": true,
+    "disabled": false,
     "level": "info",
+    "output": "$WORKDIR/sb.log",
     "timestamp": true
   },
   "dns": {
     "servers": [
       {
+        "tag": "cloudflare",
+        "address": "https://1.1.1.1/dns-query",
+        "strategy": "prefer_ipv4",
+        "detour": "direct"
+      },
+      {
         "tag": "google",
         "address": "tls://8.8.8.8",
-        "strategy": "ipv4_only",
+        "strategy": "prefer_ipv4",
+        "detour": "direct"
+      },
+      {
+        "tag": "quad9",
+        "address": "https://9.9.9.9/dns-query",
+        "strategy": "prefer_ipv4",
         "detour": "direct"
       }
     ],
-    "rules": [
-      {
-        "rule_set": ["geosite-openai"],
-        "server": "wireguard"
-      },
-      {
-        "rule_set": ["geosite-netflix"],
-        "server": "wireguard"
-      },
-      {
-        "rule_set": ["geosite-category-ads-all"],
-        "server": "block"
-      }
-    ],
-    "final": "google",
-    "strategy": "",
+    "final": "$fastest_dns",
+    "strategy": "prefer_ipv4",
     "disable_cache": false,
     "disable_expire": false
   },
@@ -1550,78 +1579,116 @@ EOF
       "tag": "direct"
     },
     {
-      "type": "block",
-      "tag": "block"
+      "type": "direct",
+      "tag": "direct-ipv4-prefer-out",
+      "domain_strategy": "prefer_ipv4"
     },
     {
-      "type": "dns",
-      "tag": "dns-out"
+      "type": "direct",
+      "tag": "direct-ipv4-only-out",
+      "domain_strategy": "ipv4_only"
     },
     {
       "type": "wireguard",
       "tag": "wireguard-out",
-      "server": "162.159.195.100",
-      "server_port": 4500,
+      "server": "engage.cloudflareclient.com",
+      "server_port": 2408,
       "local_address": [
         "172.16.0.2/32",
-        "2606:4700:110:83c7:b31f:5858:b3a8:c6b1/128"
+        "2606:4700:110:812a:4929:7d2a:af62:351c/128"
       ],
-      "private_key": "mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=",
+      "private_key": "gBthRjevHDGyV0KvYwYE52NIPy29sSrVr6rcQtYNcXA=",
       "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-      "reserved": [26, 21, 228]
+      "mtu": 1420,
+      "reserved": [6, 146, 6]
+    },
+    {
+      "type": "direct",
+      "tag": "wireguard-ipv4-prefer-out",
+      "detour": "wireguard-out",
+      "domain_strategy": "prefer_ipv4"
+    },
+    {
+      "type": "direct",
+      "tag": "wireguard-ipv4-only-out",
+      "detour": "wireguard-out",
+      "domain_strategy": "ipv4_only"
     }
   ],
   "route": {
-    "rules": [
-      {
-        "protocol": "dns",
-        "outbound": "dns-out"
-      },
-      {
-        "ip_is_private": true,
-        "outbound": "direct"
-      },
-      {
-        "rule_set": ["geosite-openai"],
-        "outbound": "wireguard-out"
-      },
-      {
-        "rule_set": ["geosite-netflix"],
-        "outbound": "wireguard-out"
-      },
-      {
-        "rule_set": ["geosite-category-ads-all"],
-        "outbound": "block"
-      }
-    ],
     "rule_set": [
       {
         "tag": "geosite-netflix",
         "type": "remote",
         "format": "binary",
         "url": "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-netflix.srs",
-        "download_detour": "direct"
+        "update_interval": "1d"
       },
       {
         "tag": "geosite-openai",
         "type": "remote",
         "format": "binary",
         "url": "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo/geosite/openai.srs",
-        "download_detour": "direct"
+        "update_interval": "1d"
+      }
+    ],
+    "rules": [
+      {
+        "rule_set": [
+          "geosite-netflix"
+        ],
+        "outbound": "wireguard-ipv4-only-out"
       },
       {
-        "tag": "geosite-category-ads-all",
-        "type": "remote",
-        "format": "binary",
-        "url": "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-category-ads-all.srs",
-        "download_detour": "direct"
+        "domain": [
+          "api.statsig.com",
+          "browser-intake-datadoghq.com",
+          "cdn.openai.com",
+          "chat.openai.com",
+          "auth.openai.com",
+          "chat.openai.com.cdn.cloudflare.net",
+          "ios.chat.openai.com",
+          "o33249.ingest.sentry.io",
+          "openai-api.arkoselabs.com",
+          "openaicom-api-bdcpf8c6d2e9atf6.z01.azurefd.net",
+          "openaicomproductionae4b.blob.core.windows.net",
+          "production-openaicom-storage.azureedge.net",
+          "static.cloudflareinsights.com"
+        ],
+        "domain_suffix": [
+          ".algolia.net",
+          ".auth0.com",
+          ".chatgpt.com",
+          ".challenges.cloudflare.com",
+          ".client-api.arkoselabs.com",
+          ".events.statsigapi.net",
+          ".featuregates.org",
+          ".identrust.com",
+          ".intercom.io",
+          ".intercomcdn.com",
+          ".launchdarkly.com",
+          ".oaistatic.com",
+          ".oaiusercontent.com",
+          ".observeit.net",
+          ".openai.com",
+          ".openaiapi-site.azureedge.net",
+          ".openaicom.imgix.net",
+          ".segment.io",
+          ".sentry.io",
+          ".stripe.com"
+        ],
+        "domain_keyword": [
+          "openaicom-api"
+        ],
+        "outbound": "wireguard-ipv4-prefer-out"
       }
     ],
     "final": "direct"
   },
   "experimental": {
     "cache_file": {
-      "path": "cache.db",
+      "enabled": true,
+      "path": "$WORKDIR/cache.db",
       "cache_id": "mycacheid",
       "store_fakeip": true
     }
